400dd02becf0f031d88fa2f07a45414c1c4880bf
Suyesh Bhandari <suyeshb@gmail.com>
2019-06-23 15:10:48 -0400

    NavBar layout

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { Button, Image } from "semantic-ui-react";
+import Color from "color";
+import TrelloLogo from "../../assets/images/trelloLogo.svg";
 
 const NavBarContainer = styled.div`
   width: 100%;
-  height: 55px;
+  height: 45px;
   display: flex;
   flex-direction: row;
   background-color: #016aa7;
@@ -13,17 +16,54 @@ const NavBarContainer = styled.div`
 `;
 
 const NavSection = styled.div`
-  background-color: ${props => props.background || "red"};
+  background-color: transparent;
   height: 100%;
   width: 100%;
+  display: flex;
+  align-items: center;
+  padding-left: 5px;
+  padding-right: 5px;
+  justify-content: ${props => props.justifyContent || "center"};
+`;
+
+const StyledButton = styled(Button)`
+  background-color: #599ac3 !important;
+  border: none !important;
+  box-shadow: 0 !important;
+
+  &:hover {
+    background-color: ${Color("#599ac3")
+      .darken(0.1)
+      .hex()} !important;
+  }
+`;
+
+const StyledImage = styled(Image)`
+  width: 7rem;
+  opacity: 0.5;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.9;
+  }
 `;
 
 function NavBar() {
   return (
     <NavBarContainer>
-      <NavSection background="blue" />
-      <NavSection background="green" />
-      <NavSection />
+      <NavSection justifyContent="flex-start">
+        <StyledButton icon="home" color="facebook" name="home" />
+        <StyledButton icon="trello" color="facebook" name="boards" />
+        <StyledButton icon="search" color="facebook" name="search" />
+      </NavSection>
+      <NavSection>
+        <StyledImage src={TrelloLogo} />
+      </NavSection>
+      <NavSection justifyContent="flex-end">
+        <StyledButton icon="add" color="facebook" name="add" />
+        <StyledButton icon="bell" color="facebook" name="notifications" />
+        <StyledButton icon="search" color="facebook" name="profile" />
+      </NavSection>
     </NavBarContainer>
   );
 }
