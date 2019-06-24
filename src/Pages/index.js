20aa4f72797dde72b3d103ee6ce7fd117647e368
Suyesh Bhandari <suyeshb@gmail.com>
2019-06-24 12:13:53 -0400

    search route

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,11 +3,13 @@ import { lazy } from "react";
 const Boards = lazy(() => import("./Boards"));
 const Board = lazy(() => import("./Board"));
 const NoMatch = lazy(() => import("./NoMatch"));
+const Search = lazy(() => import("./Search"));
 
 const Pages = {
   Boards,
   Board,
-  NoMatch
+  NoMatch,
+  Search
 };
 
 export default Pages;
