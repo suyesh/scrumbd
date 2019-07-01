8fce9101912bd4928e6a6d34cd0b2ee2857ee8d8
Suyesh Bhandari <suyeshb@gmail.com>
2019-07-01 11:25:20 -0400

    Boards grid

--- src/Pages/Boards.jsx
+++ src/Pages/Boards.jsx
@@ -4,19 +4,19 @@ import { media, sizeREM } from "../utils/deviceSizes";
 
 const BoardsContainer = styled.div`
   height: 100%;
-  margin: 2% 2%;
+  margin: 2% 1%;
   display: grid;
   grid-template-columns: 1fr;
-  grid-template-rows: minmax(calc(100vh - 100px), 100%);
+  grid-template-rows: minmax(calc(100vh - 6.25rem), 100%);
   @media ${media.tablet} {
     margin: 2% 8%;
     grid-gap: 1.429rem;
     grid-template-columns: 16.429rem 1fr;
   }
 
-  @media (min-width: 62.714rem) and (max-width: 78.571rem) {
+  @media (min-width: 54.938rem) and (max-width: 78.571rem) {
     grid-gap: 0.714rem;
-    margin: 1%;
+    margin: 2% 0.5%;
   }
 `;
 
