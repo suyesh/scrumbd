cce95836ca06ad71770345b12c8769318871ae70
Suyesh Bhandari <suyeshb@gmail.com>
2019-07-03 09:48:33 -0400

    Modularized

--- src/containers/DashboardNav/index.jsx
+++ src/containers/DashboardNav/index.jsx
@@ -6,17 +6,19 @@ import { SideMenu, StyledUl, StyledLi, StyledSpan } from "../../components";
 import { ROUTES } from "../../constants";
 
 function DashboardNavBase({ location: { pathname } }) {
+  const home = pathname === ROUTES.root;
+  const boards = pathname === ROUTES.boards;
   return (
     <SideMenu>
       <StyledUl>
         <Link to={ROUTES.root}>
-          <StyledLi active={pathname === ROUTES.root}>
+          <StyledLi active={home}>
             <Icon name="home" />
             <StyledSpan>Home</StyledSpan>
           </StyledLi>
         </Link>
         <Link to={ROUTES.boards}>
-          <StyledLi active={pathname === ROUTES.boards}>
+          <StyledLi active={boards}>
             <Icon name="trello" />
             <StyledSpan>Boards</StyledSpan>
           </StyledLi>
