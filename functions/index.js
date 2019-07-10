eb59bdbeb9980896e0f3be4f37ec4376f72456bb
Suyesh Bhandari <suyeshb@gmail.com>
2019-07-10 13:27:24 -0400

    .

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,7 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.createUser = functions.auth.user().onCreate(user => {
+  console.log(user);
+});
