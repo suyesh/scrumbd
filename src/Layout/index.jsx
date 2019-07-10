acb4677dd7694d7f46d22f9bad3ce4dbf2602149
Suyesh Bhandari <suyeshb@gmail.com>
2019-07-10 13:35:20 -0400

    .

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,19 +1,28 @@
 import React, { Fragment, useEffect } from "react";
 import { NavBar } from "../containers";
 import GlobalStyle from "../utils/globalStyles";
+import { ROUTES } from "../constants";
 import Head from "./Head";
 
 function withLayout(Comp) {
   return function AppWithLayout(props) {
+    const redirectToDashboard = () => {
+      props.history.push(ROUTES.root);
+    };
+
+    const redirectToHome = () => {
+      props.history.push(ROUTES.home);
+    };
+
     useEffect(
       () => {
         if (props.user) {
-          props.history.push("/");
+          redirectToDashboard();
         } else {
-          props.history.push("/login");
+          redirectToHome();
         }
       },
-      [props.user, props.history]
+      [props.user, redirectToDashboard, redirectToHome]
     );
     return (
       <Fragment>
