f88150438f269f3c4ec9e2e3665efbb6935773a9
Suyesh Bhandari <suyeshb@gmail.com>
2019-07-11 11:33:18 -0400

    Scrumbd boards

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -29,7 +29,7 @@ export function NotLoggedInRoutes({
               setError={setError}
               signIn={signIn}
               signInWithGoogle={signInWithGoogle}
-              loading={loading}
+              loading={loading && !user}
             />
           )}
         />
