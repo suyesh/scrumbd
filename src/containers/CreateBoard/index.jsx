a60fc5d32b3ae4fdc1c3d429af9f195c1b92a96d
Suyesh Bhandari <suyeshb@gmail.com>
2019-07-16 21:27:23 -0400

    .

--- src/containers/CreateBoard/index.jsx
+++ src/containers/CreateBoard/index.jsx
@@ -52,25 +52,25 @@ const StyledInput = styled(Input)`
     background: transparent !important;
     border: none !important;
     color: #fff !important;
-    font-size: 16px !important;
+    font-size: 1.143rem !important;
     font-weight: 700 !important;
-    line-height: 24px !important;
+    line-height: 1.5em !important;
     margin: 0 !important;
     min-height: 0 !important;
     outline: 0 !important;
     background: hsla(0, 0%, 100%, 0.1) !important;
-    height: 35px !important;
+    height: 2.188em !important;
 
     &::placeholder {
       color: #fff !important;
-      font-size: 16px !important;
+      font-size: 1em !important;
       font-weight: 800 !important;
-      line-height: 24px !important;
+      line-height: 1.5em !important;
     }
 
     &:focus {
       background: hsla(0, 0%, 100%, 0.3) !important;
-      border-radius: 3px;
+      border-radius: 0.188em;
     }
   }
 `;
